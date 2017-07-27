e66c1be0a1808d846cadbe3e6ae60626941f5999
wasper256 <wasper256@gmail.com>
2017-07-27 15:46:37 +0300

    add db models and some admin preferences

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Author, Genre, Book, BookInstance
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
