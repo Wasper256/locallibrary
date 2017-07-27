75b8ee3715bf9621441460dd10e3eed554fdac38
wasper256 <wasper256@gmail.com>
2017-07-27 19:36:03 +0300

    something like half of tutorial completed

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Language
 from django.contrib import admin
 
 # Register your models here.
@@ -9,3 +9,78 @@ admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
+admin.site.register(Language)
+
+
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)."""
+
+    model = Book
+
+admin.site.unregister(Author)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Administration object for Author models.
+
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields), grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)."""
+
+    model = BookInstance
+
+
+class BookAdmin(admin.ModelAdmin):
+    """
+    Administration object for Book models.
+
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
+
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_description = 'Genre'
+
+admin.site.unregister(BookInstance)
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    """
+    Administration object for BookInstance models.
+
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
